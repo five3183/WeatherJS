7a7f8e64ed0fac8da6389ed45968db77dd3b4440
Matthew Curran <five3183@gmail.com>
2018-11-01 14:12:47 -0400

    updated read me, reordered files

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # WeatherJS
+
+In order to use this app. After cloning the repository, a keys.json file must be created. The file should look like this:
+{ 
+   "gMap": "yourGoogleApiKeyHere", 
+   "weather": "yourOpenWeatherMapApiKeyHere" 
+}
--- image.js
+++
@@ -1,5 +0,0 @@
-// Hurricane https://media.giphy.com/media/HmTLatwLWpTQk/giphy.gif
-//RAining https://media.giphy.com/media/cICPp2aw1sEashhIDn/giphy.gif
-//SUNNY https://media.giphy.com/media/o7R0zQ62m8Nk4/giphy.gif
-// CLOUDY https://media.giphy.com/media/qq5gwamAHVofm/giphy.gif
-// COLD DAy 
