f17895d39ffef8d6e7b07c6dbf17e361fc48c06d
Matthew Curran <five3183@gmail.com>
2019-08-01 14:13:18 -0400

    api update

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,26 +3,26 @@ const keys = new Keys()
 const coords = new Location()
 const weather = new Weather()
 const ui = new UI()
+const gKey = 'AIzaSyCx23RSeZ5399EKe1zQRiMfR2IoFgpY6WY'
+const wKey = 'dc2a70f6a1b55245b6e21c7ff70fab3c'
 
 
 // Once DOM conetent is loaded, getLocation (from local storage) => getKeys => getCoords => getWeather => paint UI
 const requestWeather = () => {
   storage.getLocationData()
     .then(location => {
-      keys.getKeys()
-        .then(keys => {
-          coords.getCoordinates(location.city, location.state, keys.key.gMap)
-            .then(locationData => {
-              const coords = locationData.results[0].geometry.location
-              const zone =  locationData.results[0].formatted_address
-              weather.getWeather(coords.lat, coords.lng, keys.key.weather)
-                .then(results => {
-                  ui.paint(zone, results)
-              })
-              .catch(err => console.log(err))
-            })
-            .catch(err => console.log(err))
+      coords.getCoordinates(location.city, location.state, gKey)
+        .then(locationData => {
+          const coords = locationData.results[0].geometry.location
+          const zone =  locationData.results[0].formatted_address
+          weather.getWeather(coords.lat, coords.lng, wKey)
+            .then(results => {
+              ui.paint(zone, results)
+          })
+          .catch(err => console.log(err))
         })
+        .catch(err => console.log(err))
+        
     })
 }
 
@@ -40,5 +40,6 @@ document.getElementById('w-change-btn').addEventListener('click', () => {
     })
   document.getElementById('w-form').reset()
   $('#location-modal').modal('hide')
+  location.reload()
  
 })
--- assets/js/ui.js
+++ assets/js/ui.js
@@ -15,14 +15,22 @@ class UI {
       let temp = (((parseInt(weather.main.temp) - 273.15) * 9/5) + 32).toFixed(0)
       // Change humidity to a percentage
       let humid = (weather.main.humidity / 100)
-      this.location.textContent = location
+      let locationArray = location.split(',')
+      let state = locationArray[1]
+      let city = locationArray[0]
+      let country = locationArray[2]
+      this.location.innerHTML = `
+         <h1>${country}</h1>
+         <h2>${city}, ${state}</h2>
+      `
       this.description.textContent = weather.weather[0].description.toUpperCase()
       this.icon.textContent = weather.weather[0].icon
+      // console.log(weather.weather[0].icon)
       // *****
-      // TO DO get better weather images
+      // TO DO update with condition code logic
       // *****
       let weatherIcon = weather.weather[0].icon
-      this.icon.setAttribute('src', `http://openweathermap.org/img/w/${weatherIcon}.png`)
+      this.icon.classList.add(`w-${weatherIcon}`)
       this.temp.textContent = temp + "°"
       this.humidity.textContent = `Relative Humidity: ${weather.main.humidity}%`
       this.dewpoint.textContent = `Dew Point: ${dewPoint(temp, weather.main.humidity)}°`
